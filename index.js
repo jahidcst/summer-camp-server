b1765dfbaa0bad4c15541091376afc28eb6e04a7
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-06-13 11:16:23 +0600

    get api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.ncm3v5o.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ncm3v5o.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,12 +26,29 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // class collection
+        const classCollection = client.db("summerdb").collection("class")
+        const instructorCollection = client.db("summerdb").collection("instructor")
+
+        // get class api
+        app.get('/class', async(req, res) =>{
+            const result = await classCollection.find().toArray();
+            res.send(result)
+        })
+
+        // get instructor api
+        app.get('/instructor', async(req, res) =>{
+            const result = await instructorCollection.find().toArray();
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
